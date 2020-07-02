4cc9a0f9151ae38d9db1a170ba3f5c8df8ac2086
Rahul <rahul78275@gmail.com>
2020-07-02 16:07:10 +0530

    model account

--- src/app/entries/account/candy-club/user-my-account/my-account.component.ts
+++ src/app/entries/account/candy-club/user-my-account/my-account.component.ts
@@ -27,6 +27,7 @@ user_profile_picture : string;
 is_content_creator : boolean;
 fileInputs : FileList;
 userId : string;
+username : string;
 constructor(private requestService : RequestService, private router : Router, private location : Location, private checkStreamerService :CheckStreamerService) {
 
     this.profile_picture = null;
@@ -59,7 +60,7 @@ constructor(private requestService : RequestService, private router : Router, pr
     }
 
     this.user_profile_picture = "../../../../assets/img/pro-img.jpg";
-    this.is_content_creator = false;
+    this.is_content_creator = true;
 }
 
   ngAfterViewInit(){
@@ -77,12 +78,12 @@ constructor(private requestService : RequestService, private router : Router, pr
      // Load Logged In User Profile
 
      setTimeout(()=>{
-
          this.user_profile_fn("userDetails", "");
          let user_id = (localStorage.getItem('userId') != '' && localStorage.getItem('userId') != null && localStorage.getItem('userId') != undefined) ? localStorage.getItem('userId') : '';
-
+         let username = (localStorage.getItem('username') != '' && localStorage.getItem('username') != null && localStorage.getItem('username') != undefined) ? localStorage.getItem('username') : '';
          this.userId = user_id;
-        this.user_notification_fn("notificationSetting", "");
+         this.username = username;
+         this.user_notification_fn("notificationSetting", "");
      }, 1000);
 
  }
@@ -92,44 +93,32 @@ user_profile_fn(url, object) {
   this.requestService.getMethod(url, object)
       .subscribe(
           (data : any ) => {
-
               if (data.success == true) {
-               // console.log(this.user_details);
                   this.user_details = data;
-                //  console.log(this.user_details);
                   this.user_profile_picture = data.picture;
-
                   this.is_content_creator = data.is_content_creator;
-
                   this.checkStreamerService.emit({is_content_creator : data.is_content_creator, user_type : data.user_type});
 
               } else {
-
                   this.errorMessages = data.error_messages;
-
                   $.toast({
                       heading: 'Error',
                       text: this.errorMessages,
-                  // icon: 'error',
                       position: 'top-right',
                       stack: false,
                       textAlign: 'left',
                       loader : false,
                       showHideTransition: 'slide'
-                  });
-                  
+                  });                  
               }
 
           },
 
           (err : HttpErrorResponse) => {
-
               this.errorMessages = 'Oops! Something Went Wrong';
-
               $.toast({
                   heading: 'Error',
                   text: this.errorMessages,
-              // icon: 'error',
                   position: 'top-right',
                   stack: false,
                   textAlign: 'left',
@@ -144,9 +133,7 @@ user_profile_fn(url, object) {
 }
 
 // get notification setting of logged in user
-
 user_notification_fn(url, object) {
-
     this.requestService.getMethod(url, object)
         .subscribe(
             (data : any ) => {                
@@ -155,14 +142,11 @@ user_notification_fn(url, object) {
                 }  
             },
   
-            (err : HttpErrorResponse) => {
-  
+            (err : HttpErrorResponse) => {  
                 this.errorMessages = 'Oops! Something Went Wrong';
-  
                 $.toast({
                     heading: 'Error',
                     text: this.errorMessages,
-                // icon: 'error',
                     position: 'top-right',
                     stack: false,
                     textAlign: 'left',
@@ -178,7 +162,6 @@ user_notification_fn(url, object) {
 
 // Email notification setting
 emailNotificationFn(form :NgForm) {
-
     this.requestService.postMethod('updateEmailNotification', form.value)
         .subscribe(
             (data : any ) => {
@@ -186,7 +169,6 @@ emailNotificationFn(form :NgForm) {
                     $.toast({
                         heading: 'Success',
                         text: "Email notification has been updated successfully",
-                    // icon: 'error',
                         position: 'top-right',
                         stack: false,
                         textAlign: 'left',
@@ -199,35 +181,28 @@ emailNotificationFn(form :NgForm) {
                     $.toast({
                         heading: 'Error',
                         text: 'Something went wrong, please try again',
-                    // icon: 'error',
                         position: 'top-right',
                         stack: false,
                         textAlign: 'left',
                         loader : false,
                         showHideTransition: 'slide'
-                    });
-                    
+                    });                    
                 }
 
             },
 
             (err : HttpErrorResponse) => {
-
                 this.errorMessages = 'Oops! Something Went Wrong';
-
                 $.toast({
                     heading: 'Error',
                     text: this.errorMessages,
-                // icon: 'error',
                     position: 'top-right',
                     stack: false,
                     textAlign: 'left',
                     loader : false,
                     showHideTransition: 'slide'
                 });
-
             }
-
         );
 }
 
@@ -240,7 +215,6 @@ webNotificationFn(form :NgForm) {
                 $.toast({
                     heading: 'Success',
                     text: "Web notification has been updated successfully",
-                // icon: 'error',
                     position: 'top-right',
                     stack: false,
                     textAlign: 'left',
@@ -253,26 +227,20 @@ webNotificationFn(form :NgForm) {
                 $.toast({
                     heading: 'Error',
                     text: 'Something went wrong, please try again',
-                // icon: 'error',
                     position: 'top-right',
                     stack: false,
                     textAlign: 'left',
                     loader : false,
                     showHideTransition: 'slide'
-                });
-                
+                });   
             }
-
         },
 
         (err : HttpErrorResponse) => {
-
             this.errorMessages = 'Oops! Something Went Wrong';
-
             $.toast({
                 heading: 'Error',
                 text: this.errorMessages,
-            // icon: 'error',
                 position: 'top-right',
                 stack: false,
                 textAlign: 'left',
@@ -294,19 +262,15 @@ changePasswordFn(form : NgForm) {
         this.requestService.postMethod('changePassword', form.value)
             .subscribe(
                 (data : any ) => {
-
                     if (data.success == true) {
-
                         localStorage.removeItem('accessToken');
-
+                        localStorage.removeItem('username');
                         localStorage.removeItem('userId');
-
                         //localStorage.clear();
 
                         $.toast({
                             heading: 'Success',
                             text: "You have been logged out successfully",
-                        // icon: 'error',
                             position: 'top-right',
                             stack: false,
                             textAlign: 'left',
@@ -315,22 +279,17 @@ changePasswordFn(form : NgForm) {
                         });
 
                         if (this.is_content_creator) {
-
                             this.router.navigate(['/']);
 
                         } else {
-
                             this.router.navigate(['/']);
                         }
 
                     } else {
-
                         this.errorMessages = data.error_messages;
-
                         $.toast({
                             heading: 'Error',
                             text: this.errorMessages,
-                        // icon: 'error',
                             position: 'top-right',
                             stack: false,
                             textAlign: 'left',
@@ -343,9 +302,7 @@ changePasswordFn(form : NgForm) {
                 },
 
                 (err : HttpErrorResponse) => {
-
                     this.errorMessages = 'Oops! Something Went Wrong';
-
                     $.toast({
                         heading: 'Error',
                         text: this.errorMessages,
@@ -369,7 +326,6 @@ changePasswordFn(form : NgForm) {
    updateProfileFn(form : NgForm) {
 
     if (form.value['name'] == undefined || form.value['name'] == '' || form.value['name'] == null) {
-
         $.toast({
             heading: 'Error',
             text: "Username should not be an Empty",
@@ -386,24 +342,20 @@ changePasswordFn(form : NgForm) {
     }
 
     if (form.value['first_name'] == undefined || form.value['first_name'] == '' || form.value['first_name'] == null) {
-
         $.toast({
             heading: 'Error',
             text: "First name should not be an Empty",
-        // icon: 'error',
             position: 'top-right',
             stack: false,
             textAlign: 'left',
             loader : false,
             showHideTransition: 'slide'
         });
-
         return false;
 
     }
 
     if (form.value['last_name'] == undefined || form.value['last_name'] == '' || form.value['last_name'] == null) {
-
         $.toast({
             heading: 'Error',
             text: "Last name should not be an Empty",
@@ -414,7 +366,6 @@ changePasswordFn(form : NgForm) {
             loader : false,
             showHideTransition: 'slide'
         });
-
         return false;
 
     }
@@ -423,9 +374,7 @@ changePasswordFn(form : NgForm) {
     this.requestService.postMethod('updateProfile', form.value)
         .subscribe(
             (data : any ) => {
-
                 if (data.success == true) {
-
                     $.toast({
                         heading: 'Success',
                         text: "Your profile has been updated successfully",
@@ -436,13 +385,10 @@ changePasswordFn(form : NgForm) {
                         loader : false,
                         showHideTransition: 'slide'
                     });
-
                     location.reload();
 
                 } else {
-
                     this.errorMessages = data.error_messages;
-
                     $.toast({
                         heading: 'Error',
                         text: this.errorMessages,
@@ -452,16 +398,13 @@ changePasswordFn(form : NgForm) {
                         textAlign: 'left',
                         loader : false,
                         showHideTransition: 'slide'
-                    });
-                    
+                    });                    
                 }
-
             },
 
             (err : HttpErrorResponse) => {
 
                 this.errorMessages = 'Oops! Something Went Wrong';
-
                 $.toast({
                     heading: 'Error',
                     text: this.errorMessages,
@@ -482,7 +425,6 @@ changePasswordFn(form : NgForm) {
 deleteAccountFn(form : NgForm) {
 
     if (form.value['password'] == undefined || form.value['password'] == '' || form.value['password'] == null) {
-
         $.toast({
             heading: 'Error',
             text: "Enter password to continue",
@@ -503,8 +445,8 @@ deleteAccountFn(form : NgForm) {
             (data : any ) => {
 
                 if (data.success == true) {
-
                     localStorage.removeItem('accessToken');
+                    localStorage.removeItem('username');
                     localStorage.removeItem('userId');
 
                     $.toast({
@@ -517,32 +459,23 @@ deleteAccountFn(form : NgForm) {
                         loader : false,
                         showHideTransition: 'slide'
                     });
-
                     this.router.navigate(['/']);
-
                 } else {
-
                     this.errorMessages = data.error_messages;
-
                     $.toast({
                         heading: 'Error',
                         text: this.errorMessages,
-                    // icon: 'error',
                         position: 'top-right',
                         stack: false,
                         textAlign: 'left',
                         loader : false,
                         showHideTransition: 'slide'
-                    });
-                    
+                    });   
                 }
-
             },
 
             (err : HttpErrorResponse) => {
-
                 this.errorMessages = 'Oops! Something Went Wrong';
-
                 $.toast({
                     heading: 'Error',
                     text: this.errorMessages,
