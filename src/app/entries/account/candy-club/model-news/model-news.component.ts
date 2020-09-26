c65cef74da0bd4ca07f815dc5d62ccfc0e728aba
RAHUL <rahul@RAHULs-Thakur.local>
2020-09-26 23:36:11 +0530

    fixing-issues-model-home

--- src/app/entries/account/candy-club/model-news/model-news.component.ts
+++ src/app/entries/account/candy-club/model-news/model-news.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../../../../common/services/request.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import {TimeAgoPipe} from 'time-ago-pipe';
+
+declare var $: any;
 
 @Component({
   selector: 'app-model-news',
@@ -7,9 +13,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelNewsComponent implements OnInit {
 
-  constructor() { }
+  errorMessages : string;
+  sliders : any[];
+  constructor(private requestService : RequestService, private router : Router) { 
+    this.errorMessages='';
+    this.sliders = [];
+  }
 
   ngOnInit() {
+    let data={type:'model'};
+    this.getModeNewsFn('getModelNews',data);
+  }
+
+  getModeNewsFn(url,object){
+    this.requestService.postMethod(url,object) 
+    .subscribe((data : any) => {
+      if (data.success == true) {
+        this.sliders = data.data;
+      } else {
+          this.errorMessages = data.error_messages;
+          this.toast_message("Error", this.errorMessages);
+      }
+    },
+      (err : HttpErrorResponse) => {
+        this.errorMessages = 'Oops! Something Went Wrong';
+        this.toast_message("Error", this.errorMessages);
+    });
+  }
+  toast_message(heading, message) {
+    $.toast({
+        heading: heading,
+        text: message,
+        position: 'top-right',
+        stack: false,
+        textAlign: 'left',
+        loader : false,
+        showHideTransition: 'slide'
+    });
   }
 
 }
