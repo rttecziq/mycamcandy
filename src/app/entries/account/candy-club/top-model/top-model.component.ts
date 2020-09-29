6df72175d137ad52f65af37602e0d5636a15a44c
RAHUL <rahul@RAHULs-Thakur.local>
2020-09-29 23:33:10 +0530

    All moder earning wise

--- src/app/entries/account/candy-club/top-model/top-model.component.ts
+++ src/app/entries/account/candy-club/top-model/top-model.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../../../../common/services/request.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+//import {TimeAgoPipe} from 'time-ago-pipe';
+
+declare var $: any;
 
 @Component({
   selector: 'app-top-model',
@@ -6,10 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-model.component.css']
 })
 export class TopModelComponent implements OnInit {
-
-  constructor() { }
+  model_id:string;
+  all_top_models:string;
+  errorMessages : string;
+  constructor(private requestService : RequestService, private router : Router) {
+    this.model_id = (localStorage.getItem('userId') != '' && localStorage.getItem('userId') != null && localStorage.getItem('userId') != undefined) ? localStorage.getItem('userId') : '';
+    this.all_top_models='';
+    this.errorMessages = '';
+   }
 
   ngOnInit() {
+    let data={model_id:this.model_id, topModelsEarning:'listAllTopModel'}
+    this.top_model_fn("topModel", data);
+  }
+  
+  top_model_fn(url, object) {  
+    this.requestService.postMethod(url, object)
+      .subscribe((data : any ) => {
+          if (data.success == true) { 
+            this.all_top_models =data.data;  
+          } else {
+            this.errorMessages = data.error_messages;
+            this.toast_message("Error", this.errorMessages);
+          }
+
+      },(err : HttpErrorResponse) => {
+        this.errorMessages = 'Oops! Something Went Wrong';
+        this.toast_message("Error", this.errorMessages);
+      });
+  }
+  toast_message(heading, message) {
+    $.toast({
+        heading: heading,
+        text: message,
+        position: 'top-right',
+        stack: false,
+        textAlign: 'left',
+        loader : false,
+        showHideTransition: 'slide'
+    });
   }
 
 }
