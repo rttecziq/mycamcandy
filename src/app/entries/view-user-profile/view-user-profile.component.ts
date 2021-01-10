cbdbb2d8b0c6a9d476786f179b9061c417b437c7
Rahul <rahul78275@gmail.com>
2021-01-10 22:58:05 +0530

    fixes follow,unfollow on about page.

--- src/app/entries/view-user-profile/view-user-profile.component.ts
+++ src/app/entries/view-user-profile/view-user-profile.component.ts
@@ -20,11 +20,17 @@ export class ViewUserProfileComponent implements OnInit {
   user_profile_picture : string;  
   user_cover_picture : string;
 
+  logged_in_userId : string;
+  is_follow : number;
+
   constructor(private requestService: RequestService, private router: Router, private route: ActivatedRoute) {
     this.member_name = "";
     this.member = {};
     this.user_profile_picture = "../../../../assets/img/pro-img.jpg";  
     this.user_cover_picture = "../../../../assets/img/bg-image.jpg";
+
+    this.logged_in_userId = this.requestService.userId;
+    this.is_follow = 0;
 
     this.user_details = {
       name : "",
@@ -75,6 +81,12 @@ export class ViewUserProfileComponent implements OnInit {
                   this.user_profile_picture = data.picture;
                   console.log(this.member);
 
+                  for (let val of data.followers) {
+                    if(val.follower_id == this.logged_in_userId) {
+                      this.is_follow = 1;
+                    }
+                  }
+
                   this.user_model_Preference_fn("userModelPreferences", {user_id:data.id});
 
                 } else if(data.error_messages == 'Model not found') {
@@ -105,6 +117,58 @@ export class ViewUserProfileComponent implements OnInit {
         this.errorMessages = 'Oops! Something Went Wrong';
         this.toast_message("Error", this.errorMessages);  
     });      
+  }
+
+  // Add follower from sidebar button
+  followUserSidebar(user_id : any) {
+        
+    let details = {follower_id : user_id};
+
+    this.requestService.postMethod('add_follower', details)
+        .subscribe(
+            (data : any ) => {
+                if (data.success == true) {
+                    this.model_profiles_fn("user_profile_details", {name:this.member_name});
+                    this.toast_message("Success", "You have now started following the User, you will be notified when the User goes live");  
+                } else {
+                    this.errorMessages = data.error_messages;
+                    this.toast_message("Error", this.errorMessages);
+                }
+
+            },
+
+            (err : HttpErrorResponse) => {  
+                this.errorMessages = 'Oops! Something Went Wrong';
+                this.toast_message("Error", this.errorMessages);
+            }
+
+        );
+
+  }
+
+  // Remove follower from sidebar button
+  unFollowUserSidebar(user_id) {
+    let details = {follower_id : user_id};
+
+    this.requestService.postMethod('remove_follower', details)
+        .subscribe(
+            (data : any ) => {
+                if (data.success == true) {
+                    this.model_profiles_fn("user_profile_details", {name:this.member_name});
+                    this.toast_message("Success", "You have now stopped following the user, you will not get any further notifications from the User");  
+                } else {
+                    this.errorMessages = data.error_messages;
+                    this.toast_message("Error", this.errorMessages);
+                }  
+            },
+
+            (err : HttpErrorResponse) => {  
+                this.errorMessages = 'Oops! Something Went Wrong';
+                this.toast_message("Error", this.errorMessages);
+            }
+
+        );
+
   }
 
 }
